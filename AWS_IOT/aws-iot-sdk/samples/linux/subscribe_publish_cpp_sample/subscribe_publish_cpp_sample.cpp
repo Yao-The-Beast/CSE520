45323d35298b0c2a11e12e5cb7adf68126dd090e
Yao-The-Beast <drtailor@DrTailors-MacBook-Pro.local>
2016-10-30 14:52:26 -0500

    ads

--- AWS_IOT/aws-iot-sdk/samples/linux/subscribe_publish_cpp_sample/subscribe_publish_cpp_sample.cpp
+++ AWS_IOT/aws-iot-sdk/samples/linux/subscribe_publish_cpp_sample/subscribe_publish_cpp_sample.cpp
@@ -264,7 +264,7 @@ int main(int argc, char **argv) {
 			if(publishCount > 0) {
 				publishCount--;
 			}
-			usleep(1000);
+			usleep(250);
 		} while(MQTT_REQUEST_TIMEOUT_ERROR == rc && (publishCount > 0 || infinitePublishFlag));
 	}
 
