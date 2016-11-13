776290c1ef6e5afc525e1bd59ff9be40564dc7aa
Yao-The-Beast <drtailor@DrTailors-MacBook-Pro.local>
2016-11-12 21:41:59 -0600

    def

--- AWS_IOT/aws-iot-sdk/samples/linux/Demo/AWS_Endpoint.cpp
+++ AWS_IOT/aws-iot-sdk/samples/linux/Demo/AWS_Endpoint.cpp
@@ -235,7 +235,8 @@ int main(int argc, char **argv) {
 
 			/***** call the sensor function to get data *****/
 			std::string sensorData = get_data();
-
+			std::cout << "PUBLISH" << std::endl;
+			std::cout << sensorData << std::endl;
 		    strcpy(cPayload,sensorData.c_str());
 			paramsQOS1.qos = QOS1;
 			paramsQOS1.payload = (void *) cPayload;
