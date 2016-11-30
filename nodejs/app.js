7e3b4e4422101ece597cd3e046c2c75aec2b55fc
Yao-The-Beast <drtailor@DrTailors-MacBook-Pro.local>
2016-11-30 13:01:14 -0600

    adds

--- nodejs/app.js
+++ nodejs/app.js
@@ -152,7 +152,7 @@ function mongodbHandler(msgContent){
 		var temperature = parseInt(msgContent[1]);
 		var light = parseInt(msgContent[2]);
 
-		console.log(msgContent);
+		console.log(humidity);
 		if (humidity == NaN || temperature == NaN || temperature == NaN){
 			return;
 		}
